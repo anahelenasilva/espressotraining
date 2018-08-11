caf6077a9aa97b356c5ece0706c904f6c94dc8df
Ana Helena <anahelenarp@hotmail.com>
2018-08-11 11:20:29 -0300

    Teste de text isDisplayed

---
+++ RecyclerView/app/src/androidTest/java/info/androidhive/recyclerview/MainActivityTest.java
@@ -0,0 +1,11 @@
+package info.androidhive.recyclerview;
+
+import org.junit.Rule;
+import android.support.test.rule.ActivityTestRule;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+}
